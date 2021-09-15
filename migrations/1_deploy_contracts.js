71f0d5332f3714cdff611ed577913c612673b241
tyler <tailinzhang1993@gmail.com>
2021-09-15 08:11:27 +0800

    update code

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,10 +1,12 @@
+const Migrations = artifacts.require("Migrations.sol");
 const ERC20 = artifacts.require("ERC20FixedSupply.sol");
 const ERC20Delegator = artifacts.require("ERC20Delegator.sol");
 const Loot = artifacts.require("Loot.sol");
 
 module.exports = async (deployer) => {
-	await deployer.deploy(Loot);
-	await deployer.deploy(ERC20, "Tether USD", "USDT",
-		'100000000000000000000000000', {gas: 5000000});
-	await deployer.deploy(ERC20Delegator);
+    await deployer.deploy(Migrations);
+    await deployer.deploy(ERC20, "Tether USD", "USDT",
+        '100000000000000000000000000', {gas: 5000000});
+    await deployer.deploy(ERC20Delegator);
+    await deployer.deploy(Loot);
 };
